f38b9055a81f4ea6b210c68441009575131de2d9
utk145 <utkarsh20241a0514@grietcollege.com>
2024-01-07 20:14:53 +0530

    client side: avatar and other fields upload to backend functionality working

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Register = () => {
+
+  const [formData, setFormData] = useState({})
+  const [file, setFile] = useState(null)
+  const [avatarProvided, setAvatarProvided] = useState(false)
+
+  const handleChange = (e) => {
+    setFormData(
+      {
+        ...formData,
+        [e.target.id]: e.target.value
+      }
+    )
+  };
+  // console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!file) {
+      console.log("No file selected");
+      return;
+    }
+    const formDataWithFile = new FormData();
+
+    // Append form data to FormData
+    for (const key in formData) {
+      formDataWithFile.append(key, formData[key]);
+    }
+
+    // Append file to FormData
+    formDataWithFile.append('avatar', file);
+    const response = await fetch("/api/v1/users/register",
+      {
+        method: "POST",
+        body: formDataWithFile
+      }
+    )
+    const data = await response.json();
+    console.log(data);
+   
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" className="border p-3 rounded-lg text-black " id='username' placeholder='Username' />
-        <input type="email" className="border p-3 rounded-lg text-black" id='email' placeholder='Email' />
-        <input type="password" className="border p-3 rounded-lg text-black" id='password' placeholder='Password' />
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <input onChange={handleChange} type="text" className="border p-3 rounded-lg text-black " id='username' placeholder='Username' />
+        <input onChange={handleChange} type="email" className="border p-3 rounded-lg text-black" id='email' placeholder='Email' />
+        <input onChange={handleChange} type="password" className="border p-3 rounded-lg text-black" id='password' placeholder='Password' />
+        {
+          !avatarProvided ? <>
+            <label className='text-yellow-500'>Upload an image for avatar</label>
+            <input type="file" name="avatar" id="avatar" onChange={(e) => { setFile(e.target.files[0]); setAvatarProvided(!avatarProvided) }} />
+          </> : <>
+            <p className='text-yellow-500'>Avatar uploaded.. Reload the page to reupload</p>
+          </>
+        }
+
         <button className='bg-[#FD356E] p-3 rounded-lg uppercase mt-6 hover:bg-pink-600 disabled:bg-[#FD356E]'>
           Register
         </button>
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:7000"
+      // jo bhi request /api se aari hogi uske peeche yeh append ho jaayega and not only that, ek proxy laga di jaayegi ki jo request aari hai woh issi url se aari hai. Soo now vite app jahaan bhi chal rahi ho, server ko lagega ki request same origin server se aari hai.
+    }
+  },
   plugins: [react()],
 })
