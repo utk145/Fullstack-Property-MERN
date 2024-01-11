0d3ff42c450d073c37a9ba309a41629ee5bbd08c
utk145 <utkarsh20241a0514@grietcollege.com>
2024-01-11 20:39:35 +0530

    client side: firebase app config and files upload functionality on firebase-storage

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { app } from '../configuration/firebase.config.js';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
 
 const CreateListing = () => {
+
+    const [files, setFiles] = useState([])
+    console.log(files);
+
+    const [formData, setFormData] = useState(
+        {
+            imageUrls: [],
+        }
+    );
+    console.log("listing page form data: ", formData);
+    const handleImageUpload = (e) => {
+        if (files.length > 0 && files.length < 7) {
+            const promises = [];
+            for (let i = 0; i < files.length; i++) {
+                promises.push(storeImage(files[i]))
+            }
+            Promise.all(promises).then((urls) => {
+                setFormData({ ...formData, imageUrls: formData.imageUrls.concat(urls) })
+            })
+        }
+    };
+
+    const storeImage = async (file) => {
+        return new Promise((resolve, reject) => {
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name;
+            const storageRef = ref(storage, fileName)
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on("state_changed",
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log(`Upload is ${progress}% done `);
+                },
+                (error) => {
+                    reject(error)
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+                        resolve(downloadUrl)
+                    })
+                }
+            )
+        })
+    }
+
     return (
         <div className='p-3 max-w-4xl mx-auto '>
             <h1 className='text-4xl text-center my-5 font-semibold'>Create a Listing</h1>
@@ -59,8 +106,8 @@ const CreateListing = () => {
                 <div className='flex flex-col flex-1 text-white gap-4'>
                     <p className='font-semibold'>Images: <span className='font-normal text-gray-400 ml-2'>The first image will be the cover (max 6) </span></p>
                     <div className="flex gap-4">
-                        <input type="file" id='images' accept='image/*' multiple className='p-3 border border-gray-700 rounded-lg w-full' />
-                        <button className='p-3 border border-fuchsia-600 text-fuchsia-600 rounded-lg uppercase hover:shadow-2xl disabled:opacity-80'>Upload</button>
+                        <input onChange={(e) => setFiles(e.target.files)} type="file" id='images' accept='image/*' multiple className='p-3 border border-gray-700 rounded-lg w-full' />
+                        <button type='button' className='p-3 border border-fuchsia-600 text-fuchsia-600 rounded-lg uppercase hover:shadow-2xl disabled:opacity-80' onClick={handleImageUpload}>Upload</button>
                     </div>
                     <button className='bg-fuchsia-900 p-3 rounded-lg uppercase font-semibold text-center hover:opacity-95'>Create Listing</button>
                 </div>
