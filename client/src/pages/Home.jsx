f479f864925b1dd08aac6535ad27fab5b28d4231
utk145 <utkarsh20241a0514@grietcollege.com>
2024-01-14 09:56:34 +0530

    client side: fix: code-reduction in get current user and backend: changing post to get

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,14 @@ import React, { useEffect } from 'react'
 const Home = () => {
   // useEffect(() => {
   //   async function currUser() {
+  //     const resp = await fetch("/api/v1/users/current-user")
+  //     const data = await resp.json()
+  //     console.log("cuureent user is ", data)
+  //   }
+  //   currUser()
+  // }, [])
+  // useEffect(() => {
+  //   async function currUser() {
   //     const resp = await fetch("/api/v1/users/current-user", {
   //       method: "POST",
   //       headers: {
